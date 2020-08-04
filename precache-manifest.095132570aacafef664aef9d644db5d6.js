0b56c57dca73530d094fca98464b9421e02621c3
Ritik Soni <34769340+ritik07@users.noreply.github.com>
2020-08-04 19:29:21 +0530

    Updates

---
+++ precache-manifest.095132570aacafef664aef9d644db5d6.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "312348a7e446ac045309964cff6a954e",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "587942761044f79e1f6c",
+    "url": "/portfolio-react/static/css/2.7b7e20e9.chunk.css"
+  },
+  {
+    "revision": "d11876d07ce7176c2523",
+    "url": "/portfolio-react/static/css/main.83aa18d1.chunk.css"
+  },
+  {
+    "revision": "587942761044f79e1f6c",
+    "url": "/portfolio-react/static/js/2.178ae363.chunk.js"
+  },
+  {
+    "revision": "5f4bf1dbc62428faab1b37b92f18fbbd",
+    "url": "/portfolio-react/static/js/2.178ae363.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d11876d07ce7176c2523",
+    "url": "/portfolio-react/static/js/main.2950c0e0.chunk.js"
+  },
+  {
+    "revision": "feb6fec8f95565f3aa5d",
+    "url": "/portfolio-react/static/js/runtime-main.c94dc782.js"
+  },
+  {
+    "revision": "4657d4ce8f031f68756fee7e8e67f8e2",
+    "url": "/portfolio-react/static/media/Capture.4657d4ce.PNG"
+  },
+  {
+    "revision": "ba5465be3e40fdb69e0deef360705825",
+    "url": "/portfolio-react/static/media/ajitminerals.ba5465be.PNG"
+  },
+  {
+    "revision": "4b773988619a806bf64321f287587a5b",
+    "url": "/portfolio-react/static/media/download.4b773988.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio-react/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "bce3a90649a26e9740fd50621189d924",
+    "url": "/portfolio-react/static/media/gr2.bce3a906.PNG"
+  },
+  {
+    "revision": "4efc7cb440e4eb19f45faca8b77d29bb",
+    "url": "/portfolio-react/static/media/grtextile.4efc7cb4.PNG"
+  },
+  {
+    "revision": "abc5f083166c46f1381f037c430a8258",
+    "url": "/portfolio-react/static/media/reckon2.abc5f083.PNG"
+  },
+  {
+    "revision": "9d7d364746fd22b461cdf769f574f1dd",
+    "url": "/portfolio-react/static/media/shreearts.9d7d3647.PNG"
+  },
+  {
+    "revision": "df3dd19d610d68e84161906715bdeebb",
+    "url": "/portfolio-react/static/media/skillimg.df3dd19d.jpg"
+  }
+]);
